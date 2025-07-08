43b3630b826448bf184b25edaffd70474dd19b91
Kimedero <50914712+Kimedero@users.noreply.github.com>
2025-07-08 20:23:15 +0300

    Removed unused references

--- crud_app.py
+++ crud_app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from pytz import timezone
 
 app = Flask(__name__)
 Scss(app)
